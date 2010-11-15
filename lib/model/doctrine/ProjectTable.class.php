1706fa3cbe2a0e63a7b68d15911da2391a027f40
Timo Haberkern <Timo.Haberkern@shift-up.de>
2010-11-15 14:57:26 +0100

    Added filter for project totals

--- lib/model/doctrine/ProjectTable.class.php
+++ lib/model/doctrine/ProjectTable.class.php
@@ -25,15 +25,16 @@ class ProjectTable extends Doctrine_Table
                                 $date_from = date('Y-m-d 00:00:00', $days[1]);
                                 $date_to = date('Y-m-d 23:59:59', $days[7]);
                                 break;
-            case 'this_month':  $date_from = null;
-                                $date_to = null;
+            case 'this_month':  $date_from = date('Y-m-d 00:00:00', mktime(00, 00, 00, date('m'), 01));
+                                $date_to = date('Y-m-d 23:59:59', strtotime($date_from.'+1 MONTH -1 SECOND'));
                                 break;
-            case 'last_month':  $date_from = null;
-                                $date_to = null;
+            case 'last_month':  $month = strtotime('last month');
+
+                                $date_from = date('Y-m-d 00:00:00', mktime(00, 00, 00, date('m', $month), 01));
+                                $date_to = date('Y-m-d 23:59:59', strtotime($date_from.'+1 MONTH -1 SECOND'));
                                 break;
         }
 
-        echo $date_to." ---- ".$date_from;
         $query = Doctrine_Query::create()
                                    ->select('p.*, i.*, SUM(i.value) as total')
                                    ->from('Project p')
