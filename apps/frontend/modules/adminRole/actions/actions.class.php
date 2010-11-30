ce1621112e71712f9b710b1108edd23eeca48e88
Timo Haberkern <Timo.Haberkern@shift-up.de>
2010-11-30 10:22:55 +0100

    Added Admin-Modules

---
+++ apps/frontend/modules/adminRole/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * adminRole actions.
+ *
+ * @package    timeboxx
+ * @subpackage adminRole
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class adminRoleActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
---
+++ apps/frontend/modules/adminUser/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * adminUser actions.
+ *
+ * @package    timeboxx
+ * @subpackage adminUser
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class adminUserActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
